bd02a31a0f9d58386733cdf0493a45ea50e241c3
Mari-b4 <58375104+Mari-b4@users.noreply.github.com>
2020-01-31 02:54:01 -0300

    Create trabalho 03-(02).h
    falta ainda

---
+++ trabalho 03-(02).h
@@ -0,0 +1,10 @@
+#ifndef ORDENACAO_H_INCLUDED
+#define ORDENACAO_H_INCLUDED
+
+void ordenar_insercao(int A[], int tamanho);
+void ordenar_shellsort02(int v[], int tam);
+void ordenar_bolha(int A[], int tam);
+void bucket_sort(int A[]);
+void ordenar_selecao(int A[], int tamanho);
+
+#endif // ORDENACAO_H_INCLUDED
