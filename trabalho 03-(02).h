9b4123c8d59ba275c5de298d59bd8f393a1600cd
Mari-b4 <58375104+Mari-b4@users.noreply.github.com>
2020-02-01 02:27:02 -0300

    Update trabalho 03-(02).h

--- trabalho 03-(02).h
+++ trabalho 03-(02).h
@@ -1,10 +1,15 @@
 #ifndef ORDENACAO_H_INCLUDED
 #define ORDENACAO_H_INCLUDED
 
+struct tipo_lista lista;
+
 void ordenar_insercao(int A[], int tamanho);
 void ordenar_shellsort02(int v[], int tam);
 void ordenar_bolha(int A[], int tam);
 void bucket_sort(int A[]);
 void ordenar_selecao(int A[], int tamanho);
+void bucket_sort(int A[]);
+void counting-sort(int A[], int B[], int k);
+radix-sort(int A[], int d);
 
 #endif // ORDENACAO_H_INCLUDED
